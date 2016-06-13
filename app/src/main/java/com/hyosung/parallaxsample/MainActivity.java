abfe4bd4b7272f6c656038c0abd49e941f760240
SeokbeomKim <sbkim0303@gmail.com>
2016-06-13 23:06:05 +0900

    Delete unused import.

--- app/src/main/java/com/hyosung/parallaxsample/MainActivity.java
+++ app/src/main/java/com/hyosung/parallaxsample/MainActivity.java
@@ -4,9 +4,6 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -17,14 +14,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, ItemListAdapter.OnRecyclerViewItemClickListener{
